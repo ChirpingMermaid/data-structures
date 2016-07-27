909a1603ba1ca14c7b78e414f2ae08302fcc02ad
Samuel Cheung <samhcheung@Samuels-MacBook-Air.local>
2016-07-27 12:05:41 -0700

    Complete functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,18 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    popvalue = storage[Object.keys(storage).length-1]
+    delete storage[Object.keys(storage).length-1];
+    return popvalue;
+
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

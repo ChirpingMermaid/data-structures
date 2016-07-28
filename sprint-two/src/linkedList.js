62ec1f79866d1c6b3250cc1435c7a2e57365d5b0
chirpingmermaid <bluejello@gmail.com>
2016-07-28 09:56:22 -0700

    Complete linkedList function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if ( !list.head ) {
+      list.head = newTail;
+    } else {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
   };
 
   list.removeHead = function() {
+    var value = list.head.value;
+    list.head = list.head.next;
+    return value;
+
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    var found = false;
+
+    do {
+      if (current.value === target) {
+        found = true;
+      }
+      current = current.next;
+
+    } while (current);   
+
+
+    return found;
+
   };
 
   return list;

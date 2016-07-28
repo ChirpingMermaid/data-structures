6b5cb5d342f91925f3f3bc00c7b6e8d93ffab676
chirpingmermaid <bluejello@gmail.com>
2016-07-28 14:45:53 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -12,10 +14,23 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  
+  this.children.push (Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+  if ( this.value === target ) {
+    found = true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    found = this.children[i].contains(target);
+    if (found) {
+      break;
+    }
+  }
+  return found;
 };
 
 

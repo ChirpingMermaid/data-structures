105ad729a803a229462fecaceb4555876c28af51
chirpingmermaid <bluejello@gmail.com>
2016-07-29 20:32:59 -0700

    changed hashTable to work with double

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,16 +3,16 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._filled = 0;
+  this._tuples = 0;
 };
-
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var buckets = this._storage.get(index);
-  if ( !buckets ) {
+  if (!buckets) {
     buckets = [[k, v]];
     this._storage.set(index, buckets);
+    this._tuples++;
   } else {
     var found = false;
     for (var i = 0; i < buckets.length; i++) {
@@ -23,17 +23,53 @@ HashTable.prototype.insert = function(k, v) {
     }
     if (found === false) {
       this._storage.get(index).push([k, v]);
+      this._tuples++;
     }
   }
+  this._storage.each(function(key, value, collection) {
+    console.log('length', collection.length);
+    console.log('index', key[0]);
+    console.log('value', value);
+  });
 
+  // if (this._tuples / this._limit > 0.75) {
+  //   this._limit *= 2;
+  //   var newStorage = LimitedArray(this._limit);
+  //   for ( var i = 0; i < this._limit / 2; i++) {
+  //     var current = this._storage.get(i);
+  //     if (current) {
+  //       for ( var j = 0; j < current.length; j++) {
+  //         var key = current[j][0];
+  //         var value = current[j][1];
+  //         var newIndex = getIndexBelowMaxForKey(key, this._limit);
+  //         var newCurrent = newStorage.get(newIndex);
+  //         if (!newCurrent) {
+  //           newCurrent = [[key, value]];
+  //           newStorage.set(newIndex, newCurrent);
+  //         } else {
+  //           var newfound = false;
+  //           for (var x = 0; x < newCurrent.length; x++) {
+  //             if ( key === newCurrent[x][0] ) {
+  //               newfound = true;
+  //               newCurrent[x][1] = value;
+  //             }
+  //           }
+  //           if ( newfound === false ) {
+  //             newStorage.get(newIndex).push([key, value]);
+  //           } 
+  //         }
 
-  
+  //       }
+
+  //     }
+  //   }
+  //   this._storage = newStorage;
+  // }  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var buckets = this._storage.get(index);
-
   for (var i = 0; i < buckets.length; i++) {
     if (k === buckets[i][0]) {
       return buckets[i][1];
@@ -43,11 +79,11 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._filled--;
   var buckets = this._storage.get(index);
   for (var i = 0; i < buckets.length; i++) {
     if ( k === buckets[i][0]) {
       buckets.splice(i, 1);
+      this._tuples--;
     }
   }
 
